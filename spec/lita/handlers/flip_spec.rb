238cc021912de42b11677bbadccba21973d5e117
Zac Stewart <zgstewart@gmail.com>
2014-03-12 17:21:51 -0400

    Add test for with/without "me"

--- spec/lita/handlers/flip_spec.rb
+++ spec/lita/handlers/flip_spec.rb
@@ -1,12 +1,22 @@
 require "spec_helper"
 
 describe Lita::Handlers::Flip, lita_handler: true do
+
   it { routes_command('flip me Nerdbot').to(:flip) }
   it { routes_command('flip Nerdbot').to(:flip) }
 
-  it 'flips the words' do
-    send_command('flip me Nerdbot ftw')
-    expect(replies.last).to eq('(╯°□°）╯︵ ʍʇɟ ʇoqpɹǝu')
+  context 'with a "me"' do
+    it 'flips the words' do
+      send_command('flip me Nerdbot ftw')
+      expect(replies.last).to eq('(╯°□°）╯︵ ʍʇɟ ʇoqpɹǝu')
+    end
+  end
+
+  context 'without a "me"' do
+    it 'flips the words' do
+      send_command('flip Nerdbot ftw')
+      expect(replies.last).to eq('(╯°□°）╯︵ ʍʇɟ ʇoqpɹǝu')
+    end
   end
 
 end
